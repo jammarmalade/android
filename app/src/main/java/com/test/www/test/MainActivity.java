6e2f1d2c8a2474c691996ccf015f06aa86f6ba62
jam00 <jammarmalade@163.com>
2016-05-30 11:13:35 +0800

    活动生命周期

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -13,90 +13,94 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.net.Inet4Address;
 
+public class MainActivity extends Activity {
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        Button btn_ok = (Button)findViewById(R.id.btn_ok);
-        btn_ok.setOnClickListener(new View.OnClickListener() {
+        Button startSecond = (Button)findViewById(R.id.start_second_activity);
+        Button startThird = (Button)findViewById(R.id.start_third_activity);
+        startSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //弹框显示信息
-//                Toast.makeText(MainActivity.this,"you clicked btn_ok ",Toast.LENGTH_SHORT).show();
-                //启动第二个活动
-//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//                startActivity(intent);
-                //隐式调用
-//                Intent intent = new Intent("com.test.mainactivity.ACTION_START");
-//                //可添加多个 category
-//                intent.addCategory("com.test.mainactivity.MY_CATEGORY");
-//                startActivity(intent);
-                //调用其他程序的 活动
-//                Intent intent = new Intent(Intent.ACTION_VIEW);
-//                intent.setData(Uri.parse("https://www.baidu.com/"));
-//                startActivity(intent);
-                //向下一个活动传递数据
-//                String data = "Hello Jam";
-//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//                intent.putExtra("extra_data",data);
-//                startActivity(intent);
-                //返回启动活动销毁后返回的数据
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                //请求码，用于在回调中判断数据来源
-                //返回后会调用 onActivityResult() 方法
-                startActivityForResult(intent,1);
-
+                Intent intent = new Intent(MainActivity.this , SecondActivity.class);
+                startActivity(intent);
             }
         });
-
-    }
-    /**
-     * 重写菜单
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    /**
-     * 菜单响应事件
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.add_item:
-                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-        //销毁一个活动
-//        finish();
-        return true;
-    }
-
-    /**
-     * 用于获取下一个活动返回的数据
-     * @param requestCode   启动活动时传入的请求码
-     * @param resultCode    返回数据时传入的处理结果
-     * @param data          携带返回数据的 Intent
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        switch (requestCode){
-            case 1:
-                if(resultCode == RESULT_OK){
-                    String returnData = data.getStringExtra("data_return");
-                    Log.d("MainActivity",returnData);
-                }
-                break;
-            default:
+        startThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+        //判断活动被销毁之前是否有保存数据(onSaveInstanceState方法调用)
+        if(savedInstanceState != null){
+            String tmpData = savedInstanceState.getString("data");
+            Log.d(TAG,tmpData);
         }
     }
-
-
+    /**
+     * 在活动由不可见变为可见的时候调用
+     */
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    /**
+     * 在活动准备好和用户进行交互的时候调用
+     */
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    /**
+     * 在系统准备去启动或恢复另一个活动的时候调用
+     */
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    /**
+     * 在活动完全不可见的时候调用
+     */
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    /**
+     * 在活动被销毁之前调用
+     */
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    /**
+     * 在活动由停止状态变为运行状态之前调用
+     */
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+    /**
+     * 在活动被回收之前调用
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        String tmpData = "save some data";
+        outState.putString("data",tmpData);
+        Log.d(TAG,"onSaveInstanceState");
+    }
 }
--- app/src/main/java/com/test/www/test/SecondActivity.java
+++ app/src/main/java/com/test/www/test/SecondActivity.java
@@ -17,33 +17,7 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.second_layout);
-        //接收上一个活动传递过来的值
-//        final Intent intent = getIntent();
-//        String data = intent.getStringExtra("extra_data");
-//        Log.d("SecondActivity",data);
-        //绑定单击事件，并返回数据给上一个活动
-        Button btn2 = (Button)findViewById(R.id.btn_2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent();
-                intent1.putExtra("data_return", "Hello MainActivity");
-                //用于向上一个活动返回数据
-                setResult(RESULT_OK, intent1);
-                finish();
-            }
-        });
+
     }
 
-    /**
-     * 用户点击back键返回时也返回数据
-     */
-    @Override
-    public void onBackPressed(){
-        Intent intent1 = new Intent();
-        intent1.putExtra("data_return", "Hello MainActivity from back");
-        //用于向上一个活动返回数据
-        setResult(RESULT_OK, intent1);
-        finish();
-    }
 }
