9b400d5c76abd58d6aefa519df213cc83578ffbb
jam00 <jammarmalade@163.com>
2016-05-30 15:17:03 +0800

    android 基本空间

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -2,6 +2,9 @@ package com.test.www.test;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,91 +14,77 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import java.net.Inet4Address;
 
 public class MainActivity extends BaseActivity {
     public static final String TAG = "MainActivity";
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"Main Task id is "+getTaskId());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        Button startSecond = (Button)findViewById(R.id.start_second_activity);
-        startSecond.setOnClickListener(new View.OnClickListener() {
+        editText = (EditText)findViewById(R.id.edit_text);
+        imageView = (ImageView)findViewById(R.id.image_view);
+        Button button = (Button)findViewById(R.id.button);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this , SecondActivity.class);
-//                startActivity(intent);
-                //最佳的启动活动的方法
-                SecondActivity.actionStart(MainActivity.this, "this is test");
+                switch (v.getId()){
+                    case R.id.button:
+                        //弹出输入框输入的值
+//                        String inputString = editText.getText().toString();
+//                        Toast.makeText(MainActivity.this,inputString,Toast.LENGTH_SHORT).show();
+                        //动态修改imageView 的值
+//                        imageView.setImageResource(R.drawable.t_wb);
+                        //设置进度条的可见性
+//                        if(progressBar.getVisibility() == View.GONE){
+//                            progressBar.setVisibility(View.VISIBLE);
+//                        }else{
+//                            progressBar.setVisibility(View.GONE);
+//                        }
+                        //设置进度条的进度
+//                        int progress = progressBar.getProgress();
+//                        progress = progress + 10;
+//                        progressBar.setProgress(progress);
+                        //弹出警告框（屏蔽其他交互功能）
+//                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+//                        dialog.setTitle("This is Dialog");
+//                        dialog.setMessage("Please type your phone number");
+//                        dialog.setCancelable(false);
+//                        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                //点击 ok 后执行
+//                            }
+//                        });
+//                        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                //点击 Cancel 后执行
+//                            }
+//                        });
+//                        dialog.show();
+                        //进度条对话框
+                        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                        progressDialog.setTitle("This is ProgressDialog");
+                        progressDialog.setMessage("Loading");
+                        progressDialog.setCancelable(true);
+                        progressDialog.show();
+                        break;
+                    default:
+                        break;
+                }
             }
         });
+    }
 
-        //判断活动被销毁之前是否有保存数据(onSaveInstanceState方法调用)
-        if(savedInstanceState != null){
-            String tmpData = savedInstanceState.getString("data");
-            Log.d(TAG,tmpData);
-        }
-    }
-    /**
-     * 在活动由不可见变为可见的时候调用
-     */
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d(TAG,"onStart");
-    }
-    /**
-     * 在活动准备好和用户进行交互的时候调用
-     */
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(TAG,"onResume");
-    }
-    /**
-     * 在系统准备去启动或恢复另一个活动的时候调用
-     */
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d(TAG,"onPause");
-    }
-    /**
-     * 在活动完全不可见的时候调用
-     */
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d(TAG,"onStop");
-    }
-    /**
-     * 在活动被销毁之前调用
-     */
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG,"onDestroy");
-    }
-    /**
-     * 在活动由停止状态变为运行状态之前调用
-     */
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-        Log.d(TAG,"onRestart");
-    }
-    /**
-     * 在活动被回收之前调用
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        String tmpData = "save some data";
-        outState.putString("data",tmpData);
-        Log.d(TAG,"onSaveInstanceState");
-    }
 }
