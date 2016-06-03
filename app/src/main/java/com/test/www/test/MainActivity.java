075b7ad4951efc9b261ed762100ecd3f0a98a399
jam00 <jammarmalade@163.com>
2016-06-03 18:34:23 +0800

    使用 HttpURLConnection

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -3,30 +3,93 @@ package com.test.www.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity implements View.OnClickListener{
     public static final String TAG = "MainActivity";
-    private WebView webView;
+    public static final int SHOW_RESPONSE = 0;
+    private Button sendRequest;
+    private TextView responseText;
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_RESPONSE:
+                    String response = (String) msg.obj;
+                    // 在这里进行UI操作，将结果显示到界面上
+                    responseText.setText(response);
+                    Log.d(TAG,response);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-        webView = (WebView) findViewById(R.id.web_view);
-        // getSettings()方法可以去设置一些浏览器的属性
-        webView.getSettings().setJavaScriptEnabled(true);//让 WebView 支持 JavaScript 脚本
-        webView.setWebViewClient(new WebViewClient() {
+        sendRequest = (Button) findViewById(R.id.send_request);
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.send_request) {
+            sendRequestWithHttpURLConnection();
+        }
+    }
+
+    private void sendRequestWithHttpURLConnection() {
+        // 开启线程来发起网络请求
+        new Thread(new Runnable() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                //这就表明当需要从一个网页跳转到另一个网页时，我们希望目标网页仍然在当前 WebView 中显示，而不是打开系统浏览器。
-                view.loadUrl(url); // 根据传入的参数再去加载新的网页
-                return true; // 表示当前WebView可以处理打开新网页的请求，不用借助系统浏览器
+            public void run() {
+                HttpURLConnection connection = null ;
+                try{
+                    /** 若是要发送数据
+                     connection.setRequestMethod("POST");
+                     DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                     out.writeBytes("username=admin&password=123456");
+                     */
+                    URL url = new URL("http://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    // 下面对获取到的输入流进行读取
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    Message message = new Message();
+                    message.what = SHOW_RESPONSE;
+                    // 将服务器返回的结果存放到Message中
+                    message.obj = response.toString();
+                    handler.sendMessage(message);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
             }
-        });
-        webView.loadUrl("http://www.baidu.com");
+        }).start();
     }
 
 }
