81bc3575d687fe4ee31773ed21e4a37c32989b61
jam <jammarmalade@163.com>
2016-06-05 02:04:47 +0800

    gps和 network 定位

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -1,17 +1,137 @@
 package com.test.www.test;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
+    private TextView positionTextView;
+    private TextView serverTextView;
+    private LocationManager locationManager;
+    private String provider;
+    //定位监听器
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // 更新当前设备的位置信息
+            showLocation(location);
+        }
+    };
+    private ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        positionTextView = (TextView) findViewById(R.id.position_text_view);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 获取所有可用的位置提供器
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            //gps 定位慢，总是返回 null
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            // 当没有可用的位置提供器时，弹出Toast提示用户
+            Toast.makeText(this, "No location provider to use", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            // 显示当前设备的位置信息
+            showLocation(location);
+        } else {
+            Log.d(TAG, "NULL");
+            positionTextView.setText("can't find location");
+        }
+
+        locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
+        //------------使用自定义的定位服务实现定位
+        serverTextView = (TextView)findViewById(R.id.server_text_view);
+        // 注册广播
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("locationAction");
+        this.registerReceiver(new LocationBroadcastReceiver(), filter);
+
+        // 启动服务
+        Intent intent = new Intent();
+        intent.setClass(this, LocationSvc.class);
+        startService(intent);
+
+        // 等待提示
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("正在定位...");
+        dialog.setCancelable(true);
+        dialog.show();
     }
 
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            // 关闭程序时将监听器移除
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    private void showLocation(Location location) {
+        String currentPosition = "latitude is " + location.getLatitude() + "\n" + "longitude is " + location.getLongitude();
+        positionTextView.setText(currentPosition);
+    }
+    //定位服务的广播
+    private class LocationBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (!intent.getAction().equals("locationAction")) return;
+            String locationInfo = intent.getStringExtra("location");
+            serverTextView.setText(locationInfo);
+            dialog.dismiss();
+            MainActivity.this.unregisterReceiver(this);// 不需要时注销
+        }
+    }
 }
