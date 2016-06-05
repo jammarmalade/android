9af44b3ee93a0854d1513a67340f7428651d7df0
jam <jammarmalade@163.com>
2016-06-05 17:14:49 +0800

    光照传感器

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -2,16 +2,45 @@ package com.test.www.test;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    private TextView lightLevel;
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // values数组中第一个下标的值就是当前的光照强度
+            float value = event.values[0];
+            lightLevel.setText("Current light level is " + value + " lx");
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+        lightLevel = (TextView) findViewById(R.id.light_level);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
     }
 
 }
