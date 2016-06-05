e81d32cfcd16da49d7bb4573a71ed49a02e30d33
jam <jammarmalade@163.com>
2016-06-05 16:17:42 +0800

    百度地图的使用(失败-可能是ak的原因)

--- app/src/main/java/com/test/www/test/MainActivity.java
+++ app/src/main/java/com/test/www/test/MainActivity.java
@@ -2,89 +2,50 @@ package com.test.www.test;
 
 
 import android.app.Activity;
-import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.Window;
 import android.widget.TextView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 
 
 public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
-    private TextView positionTextView;
-    private TextView serverTextView;
-    public static final int SHOW_LOCATION = 0;
-    private Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SHOW_LOCATION:
-                    String currentPosition = (String) msg.obj;
-                    positionTextView.setText(currentPosition);
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
+    private BMapManager manager;
+    private MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再 setContentView 方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.main);
-        positionTextView = (TextView)findViewById(R.id.position_text_view);
-        //32.7763644055,100.4338731743
-        showLocation(32.7763644055,100.4338731743);
+        //获取地图控件引用
+        mapView = (MapView) findViewById(R.id.map_view);
     }
 
-    private void showLocation(final double latitude,final double longitude) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    //  组装反向地理编码的接口地址
-                    StringBuilder url = new StringBuilder();
-                    url.append("http://maps.google.cn/maps/api/geocode/json?language=CN&latlng=");
-//                    url.append("http://192.168.1.46/xx.php?language=CN&latlng=");
-                    url.append(latitude).append(",");
-                    url.append(longitude);
-                    HttpClient httpClient = new DefaultHttpClient();
-                    HttpGet httpGet = new HttpGet(url.toString());
-                    //  在请求消息头中指定语言，保证服务器会返回中文数据
-                    httpGet.addHeader("Accept-Language", "zh-CN");
-                    HttpResponse httpResponse = httpClient.execute(httpGet);
-                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
-                        HttpEntity entity = httpResponse.getEntity();
-                        String response = EntityUtils.toString(entity, "utf-8");
-                        JSONObject jsonObject = new JSONObject(response);
-                        //  获取results 节点下的位置信息
-                        JSONArray resultArray = jsonObject.getJSONArray("results");
-                        if (resultArray.length() > 0) {
-                            JSONObject subObject = resultArray.getJSONObject(1);
-                            //  取出格式化后的位置信息
-                            String address = subObject.getString("formatted_address");
-                            Message message = new Message();
-                            message.what = SHOW_LOCATION;
-                            message.obj = address;
-                            handler.sendMessage(message);
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
     }
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mapView.onPause();
+    }
 }
+
